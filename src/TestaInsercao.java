d6372322151daf3f3f5114d20705c0d85d87e439
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-04-07 21:36:06 -0300

    utilizando try-with-resources
    utilizando try-with-resources

--- src/TestaInsercao.java
+++ src/TestaInsercao.java
@@ -7,37 +7,39 @@ import java.sql.Statement;
 public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
-		Connection con = ConnectionFactory.recuperarConexao();
-		con.setAutoCommit(false);
-		
-		try {
-			PreparedStatement st = con.prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)" ,
-					Statement.RETURN_GENERATED_KEYS);
-			
-			adicionarValores(st, "Geladeira", "Geladeira Azul");
-			adicionarValores(st, "Televisão", "TV LG");	
-			con.commit();
-			
-			st.close();
-			ConnectionFactory.fecharConexao(con);
-		} catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("Executando rollback");
-			con.rollback();
+		try(Connection con = ConnectionFactory.recuperarConexao()){
+			con.setAutoCommit(false);
+			try(PreparedStatement st = con.prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)" ,
+					Statement.RETURN_GENERATED_KEYS)) {
+				
+				adicionarValores(st, "Geladeira", "Geladeira Azul");
+				adicionarValores(st, "Televisão", "TV LG");	
+				con.commit();
+				
+				st.close();
+				ConnectionFactory.fecharConexao(con);
+			} catch(Exception e) {
+				e.printStackTrace();
+				System.out.println("Executando rollback");
+				con.rollback();
+			}
 		}
 	}
 
 	private static void adicionarValores(PreparedStatement st, String nome, String descricao) throws SQLException {
-		st.setString(1, nome);
-		st.setString(2, descricao);
-		
-		st.execute();
-		
-		ResultSet rs = st.getGeneratedKeys();
-		while(rs.next()) {
-			Integer id = rs.getInt(1);
+		try(ResultSet rs = st.getGeneratedKeys()) {
+			st.setString(1, nome);
+			st.setString(2, descricao);
 			
-			System.out.println("Id do produto inserido na base é: " + id);
+			st.execute();
+
+			while(rs.next()) {
+				Integer id = rs.getInt(1);
+				
+				System.out.println("Id do produto inserido na base é: " + id);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
